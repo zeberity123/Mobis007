1fa5d156bbaab52d6b083c9664e83a799350d8f1
zeberity123 <darudayu123@gmail.com>
2024-12-12 17:29:41 +0900

    add for n_of_file checker

--- auto_test_ubuntu/ubuntu_set_step3_4.py
+++ auto_test_ubuntu/ubuntu_set_step3_4.py
@@ -9,9 +9,10 @@ step_1_dir = f'/run/user/1000/gvfs/smb-share:server=192.168.2.1,share=fc3_nas_00
 step_1_folders = os.listdir(step_1_dir)
 files_for_step_3 = []
 pcd_already_done = []
+not_enough_files = []
 # search only input folders:
 # step_1_folders = ['20241116_110423_Osan2Chungju', '20241113_191104_Namisum2Gwangmyung']
-step_1_folders = ['20241113_191104_Namisum2Gwangmyung']
+# step_1_folders = ['20241113_191104_Namisum2Gwangmyung']
 
 
 def check_step_2(folder_dir):
@@ -34,22 +35,25 @@ for folder_name in step_1_folders:
         if len(i_split) == 5:
             i_folder_list = os.listdir(f'{step_1_dir}/{folder_name}/{i}')
             # if 'pcd' not in i_folder_list:
-            if ('8879_ldr2cam_calib.json' not in i_folder_list) and ('pcd' not in i_folder_list) and check_step_2(f'{step_1_dir}/{folder_name}/{i}'):
-                key_frame = i_split[-1]
-                fname_no_frame = f'{i_split[0]}_{i_split[1]}'
-                lidar_fname = f'{i_split[2]}_{i_split[3]}.bin'
-                head_fname = f'{i_split[2]}_{i_split[3]}.timestamp'
+            if ('8879_ldr2cam_calib.json' not in i_folder_list) and ('pcd' not in i_folder_list):
+                if check_step_2(f'{step_1_dir}/{folder_name}/{i}'):
+                    key_frame = i_split[-1]
+                    fname_no_frame = f'{i_split[0]}_{i_split[1]}'
+                    lidar_fname = f'{i_split[2]}_{i_split[3]}.bin'
+                    head_fname = f'{i_split[2]}_{i_split[3]}.timestamp'
 
-                result_dir_from_1 = f'{step_1_dir}/{folder_name}/{fname_no_frame}'
-                lh_base_dir = f'{origin_tw_root}/{folder_name}/Front/LiDAR/FRCMR_Lidar1_'
-                lidar_dir = f'{lh_base_dir}{lidar_fname}'
-                head_dir = f'{lh_base_dir}{head_fname}'
-                step_4_dir = f'{step_1_dir}/{folder_name}/{i}'
-                # print(result_dir_from_1)
-                # print(lidar_dir)
-                # print(head_dir)
-                # print(key_frame)
-                files_for_step_3.append([result_dir_from_1, lidar_dir, head_dir, key_frame, step_4_dir])
+                    result_dir_from_1 = f'{step_1_dir}/{folder_name}/{fname_no_frame}'
+                    lh_base_dir = f'{origin_tw_root}/{folder_name}/Front/LiDAR/FRCMR_Lidar1_'
+                    lidar_dir = f'{lh_base_dir}{lidar_fname}'
+                    head_dir = f'{lh_base_dir}{head_fname}'
+                    step_4_dir = f'{step_1_dir}/{folder_name}/{i}'
+                    # print(result_dir_from_1)
+                    # print(lidar_dir)
+                    # print(head_dir)
+                    # print(key_frame)
+                    files_for_step_3.append([result_dir_from_1, lidar_dir, head_dir, key_frame, step_4_dir])
+                else:
+                    not_enough_files.append(f'not enough files: {folder_name}/{i}')
 
             else:
                 pcd_already_done.append(f'pcd already exists: {folder_name}/{i}')
@@ -76,8 +80,8 @@ for i in files_for_step_3:
 diff_headfiles = [f'{i[0].split('TW/')[1]}' for i in diff_headfile_names]
 diff_filenames = [f'{i[0].split('TW/')[1]}::{i[1]}' for i in diff_headfile_names]
 
-for i in pcd_already_done:
-    print(i)
+# for i in pcd_already_done:
+#     print(i)
 
 # done_txt = '1212_temp_step_3_4.txt'
 # with open(done_txt, 'w+') as f:
@@ -91,7 +95,13 @@ for i in pcd_already_done:
 # with open(diff_filenames_txt, 'w+') as f:
 #     f.write('\n'.join(diff_filenames))
 
-for i in done:
+pcd_already_done_txt = 'pcd_already_done.txt'
+with open(pcd_already_done_txt, 'w+') as f:
+    f.write('\n'.join(pcd_already_done))
+
+print('\n')
+print('\n')
+for i in not_enough_files:
     print(i)
 
 print('\n')
